f88d56d5c09642ce7acf32e90866a656e3f1102d
nberts <nina@ninabsavage.ca>
2024-09-03 15:00:43 -0400

    Assignment 4 completed

---
+++ Assignment 4 - JS Plugins/index.js
@@ -0,0 +1,10 @@
+//challenge 2
+let myName = document.querySelector("#challenge-2");
+
+myName.innerHTML = "Nina B.";
+
+//challenge 3
+new Typewriter("#challenge-3", {
+  strings: ["Nina Bertrand"],
+  autoStart: true,
+});
